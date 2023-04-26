3b20e4f632ce4070f7ca3141989aeed261c3f614
isromero <ismaromeroortega@gmail.com>
2023-04-26 10:42:09 +0200

    libft reduced

---
+++ libft_reduced/libft.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isromero <isromero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 15:38:24 by isromero          #+#    #+#             */
+/*   Updated: 2023/04/26 10:39:01 by isromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+# include <limits.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int		ft_atoi(const char *str);
+void	ft_putstr(char *s);
+int		ft_isalpha(int c);
+#endif
